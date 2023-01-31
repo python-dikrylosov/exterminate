3e683ac3e04f4c2372b1493335328c2c9fd224e0
python_dikrylosov <48491372+python-dikrylosov@users.noreply.github.com>
2023-01-31 17:36:38 +0500

    Delete password.py

--- password.py
+++
@@ -1,8 +0,0 @@
-api_key_block_btc = "cae3-e5ba-4e69-2ee9"
-api_key_block_ltc = "1a2d-0feb-3608-582f"
-api_key_block_doge = "6fef-650a-9bf7-f254"
-SECRET_PIN = "mustberandomlygeneratedandunique" #kry2011gm
-
-TOKEN = 5181096956:AAHCz1EBHBMujQ0sI-FSty4GjOiBa3Ju-To#"5766014506:AAGU034zYpSwtNYp3bxA-_3LxqLiUz8HOoQ"#"5240943515:AAE6KxKEFZT6DG2MBpPPnqU3jjD97H8h-40"
-
-#"5197117880:AAHnkLJKCDqvmhoeIMEcT1W6kqL3o7be5Fg" # <<<  @free_expobot.: btc-client_official_bot>>>"5181096956:AAHCz1EBHBMujQ0sI-FSty4GjOiBa3Ju-To"
